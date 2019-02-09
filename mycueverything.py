a272397c0308129a678e2590dfbb1d864f6f7b65
Joshua Franklin <mail@joshfrankl.in>
2019-02-09 14:48:22 -0700

    Added campus cash and munch money to API.

--- mycueverything.py
+++ mycueverything.py
@@ -9,7 +9,11 @@ class MyCUEverything:
         self.username = _username
         self.password = _password
 
-    def get_meal_swipes(self):
+        self.meal_swipes = None
+        self.munch_money = None
+        self.campus_cash = None
+
+    def _parse_jsatech(self):
         session = requests.session()
 
         response = session.get('https://services.jsatech.com/login.php?cid=59')
@@ -30,13 +34,32 @@ class MyCUEverything:
 
         skey = session.post(action, data=payload).text.split("skey=")[1].split("&")[0]
 
-        requests.get(f'https://services.jsatech.com/login.php?skey={skey}&cid=59&fullscreen=1&wason=')
+        requests.get(f'https://services.jsatech.com/login.php?skey={skey}' +
+                     '&cid=59&fullscreen=1&wason=')
 
         sleep(4)
 
         response = requests.get(f'https://services.jsatech.com/index.php?skey={skey}&cid=59&')
-        swipes = BeautifulSoup(response.text, 'html.parser') \
-            .find('th', text='Current MP Balance:').parent \
-            .find('th', attrs={'colspan': ''}).get_text()
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        return int(float(swipes))
+        self.meal_swipes = int(float(soup.find('th', text='Current MP Balance:').parent
+                                         .find('th', attrs={'colspan': ''}).get_text()))
+        self.munch_money = int(float(soup.find('th', text='Current MM Balance:').parent
+                                         .find('th', attrs={'colspan': ''}).get_text()))
+        self.campus_cash = int(float(soup.find('th', text='Current CC Balance:').parent
+                                         .find('th', attrs={'colspan': ''}).get_text()))
+
+    def get_meal_swipes(self):
+        if self.meal_swipes is None:
+            self._parse_jsatech()
+        return self.meal_swipes
+
+    def get_munch_money(self):
+        if self.munch_money is None:
+            self._parse_jsatech()
+        return self.munch_money
+
+    def get_campus_cash(self):
+        if self.campus_cash is None:
+            self._parse_jsatech()
+        return self.campus_cash
