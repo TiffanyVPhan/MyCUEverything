28532e5d93addcabb0d85bcd43524b03645683f8
Joshua Franklin <joshuafranklin26@gmail.com>
2019-02-09 21:53:28 -0700

    Started dashboard

--- front-end/app.py
+++ front-end/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, session
-from flask.ext.session import Session
+from flask import Flask, render_template, request, session, redirect
+
+from api.mycueverything import MyCUEverything
 
 app = Flask(__name__)
-Session(app)
+app.secret_key = 'dev'
+
+everythings = {}
 
 
 @app.route('/')
@@ -14,6 +17,15 @@ def index():
 def login():
     if request.method == 'GET':
         return render_template('login.html')
+
+    everythings[request.cookies['session']] = MyCUEverything(request.form['username'],
+                                                             request.form['password'])
+    return redirect('dash')
+
+
+@app.route('/dash')
+def dash():
+    return render_template('dash.html', everything=everythings[request.cookies['session']])
 
 
 if __name__ == '__main__':
