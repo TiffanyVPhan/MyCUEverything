ecf64ba419c718e1212ad072f437473f08ca8628
Joshua Franklin <joshuafranklin26@gmail.com>
2019-02-09 21:07:36 -0700

    Converted to templating.

--- front-end/app.py
+++ front-end/app.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ front-end/static/js/t9hackify.js
@@ -0,0 +1,9 @@
+function t9Hackify() {
+    if (document.body.style.backgroundColor) {
+        document.body.style.backgroundColor = '';
+        document.body.style.color = 'black';
+    } else {
+        document.body.style.backgroundColor = '#b299e6';
+        document.body.style.color = 'white';
+    }
+}
