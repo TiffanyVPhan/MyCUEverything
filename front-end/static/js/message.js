9e9378062ea91ead2bab122a6b45957f00f89a3f
Joshua Franklin <joshuafranklin26@gmail.com>
2019-02-10 12:31:55 -0700

    Removed logout and error indicators when page is refreshed.

--- front-end/static/js/message.js
+++ front-end/static/js/message.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     if (window.location.href.split('?')[1] === 'error') {
         document.getElementById('alert').className = 'red';
         document.getElementById('alert-text').innerText = 'Invalid login credentials.';
-
     }
     else if (window.location.href.split('?')[1] === 'logout') {
         document.getElementById('alert').className = 'green';
         document.getElementById('alert-text').innerText = 'Successfully logged out.';
     }
+    window.history.pushState('/', 'MyCUEverything', '/');
 });
